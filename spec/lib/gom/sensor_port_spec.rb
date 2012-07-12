49f8d77f6755ae6a59987f16c69d643388a04ad3
dirk lüsebrink <dirk.luesebrink@gmail.com>
2012-07-12 12:28:09 +0200

    rspeci'fied

--- spec/lib/gom/sensor_port_spec.rb
+++ spec/lib/gom/sensor_port_spec.rb
@@ -1,49 +1,51 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe "Gom::SensorPort" do
   context "given no options" do
 
-    NODE = '/gom-sensor-port'
-
-    before :each do
-      ($gom.stub! :read).with("#{NODE}.json").and_return(<<-JSON)
+    let(:path) { '/gom-sensor-port' }
+    let(:station) { Gom::SensorPort.new "#{path}" }
+    let(:json) { <<-JSON
       {
         "node": {
-          "uri": "#{NODE}",
+          "uri": "#{path}",
           "entries": [
             { "attribute": {
               "name": "status",
-              "node": "#{NODE}",
+              "node": "#{path}",
               "value": "<void>", "type": "string"
             } }
           ]
         }
       }
-    JSON
-      @station = Gom::SensorPort.new "#{NODE}"
+      JSON
+    }
+
+    before :each do
+      ($gom.stub! :read).with("#{path}.json").and_return(json)
     end
 
     describe "#dispatch_sensor_message" do
       it "writes sensor value to gom" do
         $gom.should_receive(:write).
-          with("#{NODE}:last_sensor_message", "foo:123")
+          with("#{path}:last_sensor_message", "foo:123")
         $gom.should_receive(:write).
-          with("#{NODE}/keys:foo", "123")
-        @station.dispatch_sensor_message "foo:123"
+          with("#{path}/keys:foo", "123")
+        station.dispatch_sensor_message "foo:123"
       end
     end
 
     describe ".initialize" do
       it "has a default interface" do
-        @station.interface.should == '0.0.0.0'
+        station.interface.should == '0.0.0.0'
       end
 
       it "defaults to udp mode" do
-        @station.mode.should == :udp
+        station.mode.should == :udp
       end
 
       it "configures sensor port on 76001" do
-        @station.port.should == 44470
+        station.port.should == 44470
       end
     end
   end
